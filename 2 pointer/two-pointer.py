cccbb1d5e7df8d60691dbd3ae8477c06c9686f67
Josh Chang <coralbue@gmail.com>
2020-09-18 21:39:30 -0700

    add scene 4-5

--- 2 pointer/two-pointer.py
+++ 2 pointer/two-pointer.py
@@ -34,15 +34,15 @@ class GroupExample(Scene):
 
 class TwoPointer(Scene):
     def construct(self):
-        # 1
+        #1 show logo
         title = TextMobject("LeetDev.io", tex_to_color_map={"LeetDev": RED_C, ".io": WHITE}).scale(2)
         self.play(GrowFromCenter(title))
         
-        #2
+        #2 show 2 pointer problem
         algorithm_text = TextMobject("2 Pointer Algorithm").scale(2)
         self.play(ReplacementTransform(title, algorithm_text))
 
-        #3
+        #3 show array
         arr = []
         array_object = Group()
         prev = None
@@ -51,10 +51,10 @@ class TwoPointer(Scene):
             value = TextMobject(str(i))
             square = Square().surround(value).set_width(1).set_height(1)
             group = Group(value, square).shift(RIGHT * i)
-            # obj['value'] = value
-            # obj['square'] = square
-            # obj['group'] = group
-            # arr.append(obj)
+            obj['value'] = value
+            obj['square'] = square
+            obj['group'] = group
+            arr.append(obj)
             array_object.add(group)
 
         array_object.center()
@@ -62,4 +62,29 @@ class TwoPointer(Scene):
         # Do a better animation
         self.play(FadeOut(algorithm_text), FadeIn(array_object))
 
+        #4 show forward and back pointer move
+        front_arrow = Arrow(DOWN, UP)
+        front_arrow.scale(0.5)
+        front_arrow.next_to(arr[0]['group'], DOWN)
+
+        back_arrow = Arrow(DOWN, UP)
+        back_arrow.scale(0.5)
+        back_arrow.next_to(arr[5]['group'], DOWN)
+        arrow_runtime = 0.75
+        self.play(FadeIn(front_arrow), FadeIn(back_arrow))
+        self.play(ApplyMethod(front_arrow.next_to, arr[1]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(back_arrow.next_to, arr[4]['group'], DOWN), run_time=arrow_runtime)
+        self.play(ApplyMethod(front_arrow.next_to, arr[2]['group'], DOWN), run_time=arrow_runtime)
+        self.play(ApplyMethod(back_arrow.next_to, arr[3]['group'], DOWN), run_time=arrow_runtime)
+
+        #5 slow pointer and faster pointer move   
+        self.play(ApplyMethod(front_arrow.next_to, arr[0]['group'], DOWN, run_time=arrow_runtime), ApplyMethod(back_arrow.next_to, arr[0]['group'], DOWN, run_time=arrow_runtime))
         
+        self.play(ApplyMethod(front_arrow.next_to, arr[1]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(front_arrow.next_to, arr[2]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(front_arrow.next_to, arr[3]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(back_arrow.next_to, arr[1]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(front_arrow.next_to, arr[4]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(back_arrow.next_to, arr[2]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(back_arrow.next_to, arr[3]['group'], DOWN, run_time=arrow_runtime))
+        self.play(ApplyMethod(back_arrow.next_to, arr[4]['group'], DOWN, run_time=arrow_runtime))
