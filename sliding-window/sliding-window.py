a25fe15823e9e759bf6717ea013322ac29e5460d
Josh Chang <coralbue@gmail.com>
2020-10-23 23:15:37 -0700

    add sliding window real scripts

--- sliding-window/sliding-window.py
+++ sliding-window/sliding-window.py
@@ -1,22 +1,76 @@
 from manim import *
 
-class ShowLogo(Scene):
+class Intro(Scene):
     def construct(self):
-        img = ImageMobject('../images/logo2.png').scale(1)
-        # img.center()
+        slidingWindow = ImageMobject('image/window.png').scale(1.5).shift(LEFT*9)
 
-        self.play(GrowFromCenter(img))  # Display the image
+        self.play(ApplyMethod(slidingWindow.shift, RIGHT * 9))
+        self.wait(1)
+        text = TextMobject("Sliding Window").scale(1.5)
+        algo = TextMobject("Algorithm").scale(1.5)
+        algo.next_to(slidingWindow, UP)
+        text.next_to(algo, UP)
+        self.play(Write(text), Write(algo))
+        self.wait(1)
 
-class LikeAndSubscribe(Scene):
+class ProblemIntro(Scene):
     def construct(self):
-        like = ImageMobject('../images/like.png')
-        subscribe = ImageMobject('../images/subscribe.png').scale(0.75)
-        plus = ImageMobject('../images/plus.png').scale(0.75)
-        
-        like.next_to(like, LEFT)
-        subscribe.next_to(plus, RIGHT*4)
+        first = Text("Given an array of positive integers and a positive integer s,").scale(0.75)
+        second = Text("find the minimal length of a continuous subarray of which the sum ≥ s.").scale(0.75)
+        group = Group(first)
+        group.add(second)
+        second.next_to(first, DOWN)
+        group.center()
 
-        self.play(FadeIn(like))
-        self.play(FadeIn(plus))
-        self.play(FadeIn(subscribe))
+        self.play(Write(first, run_time=2))
+        self.play(Write(second, run_time=2))
+        self.wait(1)
 
+class AlgorithmIntro(Scene):
+    def construct(self):
+        first = Text("The sliding window algorithm is a technique that optimizes array").scale(0.75)
+        second = Text("problems by reducing a brute force O(N²) solution to be a O(N)").scale(0.75)
+        group = Group(first)
+        group.add(second)
+        second.next_to(first, DOWN)
+        group.center()
+
+        self.play(Write(first, run_time=2))
+        self.play(Write(second, run_time=2))
+        self.wait(1)
+
+class IdentifyAlgorithm(Scene):
+    def construct(self):
+        # first = Text("How to identfiy:").scale(0.75)
+        # second = Text("• Problem asks you to find the longest sequence in an").scale(0.75)
+        # third = Text("  array/string that fulfills a condition.").scale(0.75)
+        # group = Group(first)
+        # group.add(second)
+        # group.add(third)
+        # second.next_to(first, DOWN)
+        # third.next_to(second, DOWN)
+        # group.center()
+
+        # self.play(Write(first, run_time=2))
+        # self.play(Write(second, run_time=2))
+        # self.play(Write(third, run_time=2))
+        # self.wait(1)
+
+
+        first = TextMobject("How to identfiy:").scale(1.5)
+        rec = VGroup(first)
+        rec.move_to(UP*2 + LEFT*3)
+
+        third = TextMobject("• Problem asks you to find the longest sequence in an").scale(0.75)
+        four = TextMobject("array/string that fulfills a condition.").scale(0.75)
+
+        # https://www.reddit.com/r/manim/comments/iupbe8/how_to_left_align_textmobject/
+        self.play(Write(rec, run_time=2))
+        rec.add(third)
+        rec.arrange(DOWN, center=False, aligned_edge=LEFT)
+        self.play(Write(third, run_time=2))
+        rec.add(four)
+        rec.arrange(DOWN, center=False, aligned_edge=LEFT)
+        four.shift(RIGHT*0.3)
+        self.play(Write(four, run_time=2))
+        self.wait(1)
